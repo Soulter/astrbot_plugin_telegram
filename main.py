d7ec5ae17f3223aad59a7715d997ecbee7177f96
Soulter <37870767+Soulter@users.noreply.github.com>
2025-01-09 18:58:43 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from astrbot.api.all import Context
 
 class Main:
     def __init__(self, context: Context) -> None:
-        from .tg_platform_adapter import TelegramPlatformAdapter # noqa
+        from .tg_message_adapter import TelegramPlatformAdapter # noqa
