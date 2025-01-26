4235124807bd6ea5f05f5bf335f1cc1466175a3b
Soulter <37870767+Soulter@users.noreply.github.com>
2025-01-27 00:57:13 +0800

    fix: 'MessageEventResult' object has no attribute 'is_split_' #1

--- tg_message_event.py
+++ tg_message_event.py
@@ -10,21 +10,17 @@ class TelegramPlatformEvent(AstrMessageEvent):
         
     @staticmethod
     async def send_with_client(client: ExtBot, message: MessageChain, user_name: str):
-        plain_text = ""
         image_path = None
         for i in message.chain:
             if isinstance(i, Plain):
-                if message.is_split_:
-                    await client.send_message(chat_id=user_name, text=i.text)
-                plain_text += i.text
+                await client.send_message(chat_id=user_name, text=i.text)
             elif isinstance(i, Image):
                 if i.path:
                     image_path = i.path
                 else:
                     image_path = i.file
                 await client.send_photo(chat_id=user_name, photo=image_path)
-        if plain_text:
-            await client.send_message(chat_id=user_name, text=plain_text)
+
                 
         
     async def send(self, message: MessageChain):
